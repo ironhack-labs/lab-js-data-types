816a34c6850554093cf41db4ce5b04980bbdfd79
pedro roca de togores <pedrobgrdt@gmail.com>
2024-01-17 16:30:14 +0100

    finito....

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister = s1 + s2 + s3 + s4 + s5
 
 // Print out the concatenated string
 
-
+console.log(tongueTwister)
 
 
 /*******************************************
