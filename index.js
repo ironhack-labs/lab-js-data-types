a58c6af4673fdb62bdffca2c5ec59451945c2768
Hugo <hugo.maurin@hotmail.com>
2024-01-17 15:20:33 +0100

    Solved lab

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const s3 = "Ted";
 const s4 = "bread";
 const s5 = "and";
 
-// Concatenate the string variables into one new string
 
+
+// Concatenate the string variables into one new string
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 
 // Print out the concatenated string
-
+console.log(tongueTwister);
 
 
 
@@ -22,9 +24,10 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+const result = part1.slice(0,part1.length-1) + part1[part1.length-1].toUpperCase() + part2.slice(0,part2.length-1) + part2[part2.length-1].toUpperCase();
 
 // Print the cameLtaiL-formatted string
+console.log(result);
 
 
 
@@ -35,10 +38,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipAmount = billTotal * 0.15;
 
 // Print out the tipAmount
-
+console.log(tipAmount);
 
 
 
@@ -47,10 +50,10 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+let randomNumber = Math.ceil(Math.random() * 10);
 
 // Print the generated random number
-
+console.log(randomNumber);
 
 
 /*******************************************
@@ -62,15 +65,29 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+// expected False because True and False equal False
+console.log(expression1)
 
 const expression2 = a || b;
+// expected True because True or False equal True
+console.log(expression2)
 
 const expression3 = !a && b;
+// expected False because False and False equal False
+console.log(expression3)
 
 const expression4 = !(a && b);
+// expected True because the reverse of False equal True
+console.log(expression4)
 
 const expression5 = !a || !b;
+// expected True because False or True equal True
+console.log(expression5)
 
 const expression6 = !(a || b);
+// expected False because reverse of True equal False
+console.log(expression6)
 
 const expression7 = a && a;
+// expected True because True and True equal True
+console.log(expression7)
