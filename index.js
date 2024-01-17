98f4f61eeb48d261bf9dc235c6fe3466b30c634a
Gavin Alexander <gavalex7@aol.com>
2024-01-17 16:02:28 +0100

    draft

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const s5 = "and";
 // Concatenate the string variables into one new string
 
 
+const all = s1 + s2 + s3 + s4 + s5;
+console.log(all)
+
 // Print out the concatenated string
 
 
@@ -21,8 +24,17 @@ const s5 = "and";
 const part1 = "java";
 const part2 = "script";
 
-// Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+/* Convert the last letter of part1 and part2 to uppercase and concatenate the strings 
+Create a new variable named result by concatenating the strings part1 and part2, and converting their last letters to uppercase to form a new "cameLtaiL" formatted string: "javAscripT"
 
+Once done, print out the result string using console.log().*/
+
+
+var newpart1 = part1[3].toUpperCase();
+var newpart2 = part2[0].toUpperCase();
+
+var result = part1 + newpart1 + part2 + newpart2 
+console.log(result)
 
 // Print the cameLtaiL-formatted string
 
@@ -35,9 +47,11 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const newTotal = (15 / (Math.round(billTotal)) * 100)
 
 // Print out the tipAmount
+console.log(newTotal)
+
 
 
 
@@ -48,10 +62,11 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+var randomNum = Math.floor(Math.random() * 100) + 1;
 
 // Print the generated random number
 
-
+console.log(randomNum)
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -62,15 +77,25 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+//F
+console.log(expression1)
 
 const expression2 = a || b;
+//T
+console.log(expression2)
 
 const expression3 = !a && b;
-
+//T
+console.log(expression3)
 const expression4 = !(a && b);
-
+//F
+console.log(expression4)
 const expression5 = !a || !b;
-
+//T
+console.log(expression5)
 const expression6 = !(a || b);
-
+//F
+console.log(expression6)
 const expression7 = a && a;
+console.log(expression7)
+//T
