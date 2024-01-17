cd4b87ac8364c75a324e1b5611203f436a4bda71
Pauline Cvt <paulinecvt2021@gmail.com>
2024-01-17 14:53:46 +0100

    last iteration

--- index.js
+++ index.js
@@ -50,7 +50,6 @@ console.log(tipAmount);
 const randomNumber = Math.floor(Math.random() * 10);
 console.log(randomNumber);
 
-// Print the generated random number
 
 
 
@@ -63,15 +62,22 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+// false bc one of the argument is false and the const uses a &&
 
 const expression2 = a || b;
+// true because a is true
 
 const expression3 = !a && b;
+// false because !a is false and b is false
 
 const expression4 = !(a && b);
+// false because !a is false and !b is true
 
 const expression5 = !a || !b;
+// true because !b is true and it's an OR
 
 const expression6 = !(a || b);
+// true because !b is true and it's an OR
 
 const expression7 = a && a;
+// technically true but doesn't make sense in this situation
