a42820a8664a9faac4c75996f32f095c1dc6eaa3
Pauline Cvt <paulinecvt2021@gmail.com>
2024-01-17 14:46:43 +0100

    3rd exercise

--- index.js
+++ index.js
@@ -37,10 +37,11 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
+const tipAmount = (billTotal * 15)/100;
 
 // Print out the tipAmount
 
-
+console.log(tipAmount);
 
 
 /*******************************************
