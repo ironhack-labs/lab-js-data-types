95b70b568c00e0e1a1da96983a61c59cf124fcc5
Kosta Kallias <costakallias03@gmail.com>
2024-01-17 15:46:40 +0100

    day 3 js exercises

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*******************************************
     Iteration 1.1 | Tongue Twister
 *******************************************/
+
 const s1 = "Fred";
 const s2 = "fed";
 const s3 = "Ted";
@@ -8,6 +9,9 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
+const tongueTwister = s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5;
+
+console.log(tongueTwister);
 
 
 // Print out the concatenated string
@@ -22,6 +26,12 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+let modifiedString1 = part1.slice(0, -1) + part1.slice(-1).toUpperCase();
+let modifiedString2 = part2.slice(0, -1) + part2.slice(-1).toUpperCase();
+
+let result = modifiedString1 + modifiedString2;
+
+console.log(result);
 
 
 // Print the cameLtaiL-formatted string
@@ -35,7 +45,8 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+tipAmount = 0.15 * billTotal;
+console.log(tipAmount)
 
 // Print out the tipAmount
 
@@ -47,7 +58,8 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.floor(Math.random() * 10) +1;
+console.log(randomNumber)
 
 // Print the generated random number
 
@@ -62,15 +74,25 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+// false
+console.log(expression1);
 
 const expression2 = a || b;
+// true
+console.log(expression2)
 
 const expression3 = !a && b;
-
+// false
+console.log(expression3)
 const expression4 = !(a && b);
-
+// true
+console.log(expression4)
 const expression5 = !a || !b;
-
+// true
+console.log(expression5)
 const expression6 = !(a || b);
-
+// false
+console.log(expression6)
 const expression7 = a && a;
+// true
+console.log(expression7)
