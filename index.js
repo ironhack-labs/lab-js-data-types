c5e82f155f623d6e1d09e1ec6f6faeebe634d76b
Jeronimo Olea <jerooleag2004@gmail.com>
2024-01-17 14:57:06 +0100

    Done and ready!

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+let tongueTwister = s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5 + " " + s3 + " " + s2 + " " + s1 + " " + s4
 
 // Print out the concatenated string
-
+console.log(tongueTwister)
 
 
 
@@ -22,10 +22,12 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+let javaCapped = part1.slice(0, -1) + part1.charAt(part1.length - 1).toUpperCase()
+let scriptCapped = part2.slice(0, -1) + part2.charAt(part2.length - 1).toUpperCase()
+result = javaCapped + scriptCapped
 
 // Print the cameLtaiL-formatted string
-
+console.log(result)
 
 
 
@@ -35,10 +37,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipAmount = billTotal * 0.15
 
 // Print out the tipAmount
-
+console.log(tipAmount)
 
 
 
@@ -47,10 +49,10 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+randomNumber = Math.round(Math.random() * 10)
 
 // Print the generated random number
-
+console.log(randomNumber)
 
 
 /*******************************************
@@ -62,15 +64,25 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
-
+//False
 const expression2 = a || b;
-
+//True
 const expression3 = !a && b;
-
+//False
 const expression4 = !(a && b);
-
+//True
 const expression5 = !a || !b;
-
+//True
 const expression6 = !(a || b);
-
+//False
 const expression7 = a && a;
+//True
+
+// Once you are done writing down your answers, print out each expression and compare it to your answer
+console.log(expression1)
+console.log(expression2)
+console.log(expression3)
+console.log(expression4)
+console.log(expression5)
+console.log(expression6)
+console.log(expression7)
