684148f2e3927c2805145c9f8bac4b39b4ac90c9
Thomas-Deblay <thomasdelay.dev@gmail.com>
2024-01-17 14:39:54 +0100

    done

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 /*******************************************
     Iteration 1.1 | Tongue Twister
 *******************************************/
-const s1 = "Fred";
-const s2 = "fed";
-const s3 = "Ted";
-const s4 = "bread";
-const s5 = "and";
+const s1 = 'Fred';
+const s2 = 'fed';
+const s3 = 'Ted';
+const s4 = 'bread';
+const s5 = 'and';
 
 // Concatenate the string variables into one new string
-
+let tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 
 // Print out the concatenated string
-
-
-
+console.log(tongueTwister);
 
 /*******************************************
     Iteration 1.2 | Camel Tail
 *******************************************/
-const part1 = "java";
-const part2 = "script";
+const part1 = 'java';
+const part2 = 'script';
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
-
+let result =
+  part1.slice(0, part1.length - 1) +
+  part1[part1.length - 1].toUpperCase() +
+  part2.slice(0, part2.length - 1) +
+  part2[part2.length - 1].toUpperCase();
 // Print the cameLtaiL-formatted string
-
-
-
+console.log(result);
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
@@ -35,23 +34,18 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
-
+const tipAmount = billTotal * 0.15;
 // Print out the tipAmount
-
-
-
+console.log(tipAmount);
 
 /*******************************************
     Iteration 2.2 | Generate Random Number
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
-
+let randomNumber = Math.round(Math.random() * 10);
 // Print the generated random number
-
-
+console.log(randomNumber);
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -61,16 +55,23 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; //false
+console.log(expression1);
 
-const expression2 = a || b;
+const expression2 = a || b; //true
+console.log(expression2);
 
-const expression3 = !a && b;
+const expression3 = !a && b; //false
+console.log(expression3);
 
-const expression4 = !(a && b);
+const expression4 = !(a && b); //true
+console.log(expression4);
 
-const expression5 = !a || !b;
+const expression5 = !a || !b; //true
+console.log(expression5);
 
-const expression6 = !(a || b);
+const expression6 = !(a || b); // false
+console.log(expression6);
 
-const expression7 = a && a;
+const expression7 = a && a; //true
+console.log(expression7);
