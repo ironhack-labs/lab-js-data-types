40f1b0ecb759c7f7a842d7a6d7b41a16313cc5c2
Leonardo Oliveira <leooliveira1025@gmail.com>
2024-01-18 08:34:31 +0000

    data-types lab solved

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ console.log(tipAmount);
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-let randomNumber = ((Math.random() * 10) + 1);
+let randomNumber = Math.floor(Math.random() * 10) + 1;
 
 
 // Print the generated random number
