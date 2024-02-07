5317254cd3c041cfd7f15d45ff44623259e2cc54
joserobles <jarobles1018@gmail.com>
2024-02-07 17:50:35 -0400

    Lab done

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 
 // Print out the concatenated string
-
+console.log(tongueTwister);
 
 
 
@@ -22,11 +22,10 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+const result = part1.slice(0,3) + part1.slice(-1).toUpperCase() + part2.slice(0,5) + part2.slice(-1).toUpperCase();
 
 // Print the cameLtaiL-formatted string
-
-
+console.log(result);
 
 
 /*******************************************
@@ -35,11 +34,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipAmount = billTotal * .15; 
 
 // Print out the tipAmount
-
-
+console.log(tipAmount)
 
 
 /*******************************************
@@ -47,10 +45,10 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber =  Math.ceil(Math.random() *  10)
 
 // Print the generated random number
-
+console.log(randomNumber)
 
 
 /*******************************************
@@ -62,15 +60,22 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+console.log("My answer: false", `| Actual answer: ${expression1}`)
 
 const expression2 = a || b;
+console.log("My answer: true", `| Actual answer: ${expression2}`)
 
 const expression3 = !a && b;
+console.log("My answer: false", `| Actual answer: ${expression3}`)
 
 const expression4 = !(a && b);
+console.log("My answer: true", `| Actual answer: ${expression4}`)
 
 const expression5 = !a || !b;
+console.log("My answer: true", `| Actual answer: ${expression5}`)
 
 const expression6 = !(a || b);
+console.log("My answer: false", `| Actual answer: ${expression6}`)
 
 const expression7 = a && a;
+console.log("My answer: true", `| Actual answer: ${expression7}`)
