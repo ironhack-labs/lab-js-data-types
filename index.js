063810cb8b729011fa231282a23fd2bec8ca9be1
Hemyulin <daniel.budiansky@protonmail.com>
2024-02-14 14:30:17 +0100

    done

--- index.js
+++ index.js
@@ -59,7 +59,11 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
+const tipAmount = billTotal * 0.15;
+
 // Print out the tipAmount
+
+console.log(tipAmount);
 
 /*******************************************
     Iteration 2.2 | Generate Random Number
@@ -67,7 +71,11 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+
 // Print the generated random number
+
+console.log(randomNumber);
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -78,15 +86,32 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+// false
 
 const expression2 = a || b;
+// true
 
 const expression3 = !a && b;
+// false
 
 const expression4 = !(a && b);
+// true
 
 const expression5 = !a || !b;
+// true
 
 const expression6 = !(a || b);
+// false
 
 const expression7 = a && a;
+// true
+
+console.log(
+  expression1,
+  expression2,
+  expression3,
+  expression4,
+  expression5,
+  expression6,
+  expression7
+);
