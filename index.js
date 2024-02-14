a04162581f04ba39c5bb69ff0c50c3254f220137
Michael R Misruell <m.misurell@gmail.com>
2024-02-14 15:24:14 +0100

    Final answers

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+let tongueTwister = s1 + s2 + s3 + s4 + s5;
 
 // Print out the concatenated string
 
-
+console.log(tongueTwister);
 
 
 /*******************************************
@@ -22,11 +23,10 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+const part3 = part1[0].toUpperCase() + part1.slice(1,4) + " " + part2[0].toUpperCase() + part2.slice(1,6);
 
 // Print the cameLtaiL-formatted string
-
-
+console.log(part3);
 
 
 /*******************************************
@@ -35,10 +35,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+tipAmount = billTotal * .15;
 
 // Print out the tipAmount
-
+console.log(tipAmount)
 
 
 
@@ -47,10 +47,10 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.floor(Math.random() * 10);
 
 // Print the generated random number
-
+console.log(randomNumber);
 
 
 /*******************************************
@@ -62,15 +62,62 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+// false
 
 const expression2 = a || b;
+// true
 
 const expression3 = !a && b;
+// true *incorect
 
 const expression4 = !(a && b);
+// true
 
 const expression5 = !a || !b;
+// true
 
 const expression6 = !(a || b);
+// false
 
 const expression7 = a && a;
+// true
+
+if (!expression1){
+    console.log("Correct")
+}
+
+if (expression2) {
+    console.log("Correct");
+} else {
+    console.log("Incorrect");
+}
+
+if (expression3) {
+    console.log("Correct");
+} else {
+    console.log("Incorrect");
+}
+
+if (expression4) {
+    console.log("Correct");
+} else {
+    console.log("Incorrect");
+}
+
+if (expression5) {
+    console.log("Correct");
+} else {
+    console.log("Incorrect");
+}
+
+if (!expression6){
+    console.log("Correct")
+}
+
+if (expression7) {
+    console.log("Correct");
+} else {
+    console.log("Incorrect");
+}
+
+
