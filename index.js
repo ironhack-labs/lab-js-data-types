05f9c06c29761239c2fab442f3336d680340ec9f
Rafa <rafaokc33@gmail.com>
2024-02-29 19:04:08 +0100

    Iteration 2.1

--- index.js
+++ index.js
@@ -34,10 +34,10 @@ console.log(result);
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+let tipAmount = billTotal*0.15;
 
 // Print out the tipAmount
-
+console.log(tipAmount);
 
 
 
