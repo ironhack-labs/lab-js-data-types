3e9b884705e21056b494e7d99ff9231be3b22102
Rui Folgado <rui.folgado@hotmail.com>
2024-03-13 12:39:39 +0000

    solved lab

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 
 
 // Print out the concatenated string
 
-
+console.log(tongueTwister);
 
 
 /*******************************************
@@ -22,11 +23,10 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
-
+let result1 = part1 + part1.slice(3).toUpperCase();
+let result2 = part2 + part2.slice(5).toLocaleUpperCase();
 // Print the cameLtaiL-formatted string
-
-
+console.log(`${result1}${result2}`);
 
 
 /*******************************************
@@ -36,10 +36,10 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
-
+const tipAmout = billTotal * 0.15;
 // Print out the tipAmount
 
-
+console.log("the tip is " + `${tipAmout}`);
 
 
 /*******************************************
@@ -48,10 +48,11 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+let randomNumber = Math.floor((Math.random() * 10) + 1);
 
 // Print the generated random number
 
-
+console.log("the random number is " + `${randomNumber}`);
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -61,16 +62,17 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
-
-const expression2 = a || b;
-
-const expression3 = !a && b;
-
-const expression4 = !(a && b);
-
-const expression5 = !a || !b;
-
-const expression6 = !(a || b);
-
-const expression7 = a && a;
+const expression1 = a && b; // expect true
+console.log(expression1);
+const expression2 = a || b; // expect true
+console.log(expression2);
+const expression3 = !a && b; // expect false
+console.log(expression3);
+const expression4 = !(a && b); // expect true
+console.log(expression4);
+const expression5 = !a || !b;//expect true
+console.log(expression5);
+const expression6 = !(a || b);//expect false
+console.log(expression6);
+const expression7 = a && a;//expect true
+console.log(expression7);
