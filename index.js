b2f292ad8d7eb53a575ae8069f3678d059ffed0a
Fabio Niglio <fabio_niglio@hotmail.com>
2024-03-13 11:39:02 +0100

    Solved lab

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+let tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 
 // Print out the concatenated string
-
-
-
+console.log(tongueTwister);
 
 /*******************************************
     Iteration 1.2 | Camel Tail
@@ -22,12 +20,22 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+const lastLetterPart1 = part1.charAt(part1.length - 1);
 
+const lastLetterPart2 = part2.charAt(part2.length - 1);
+
+const lastLetterCapPart1 = lastLetterPart1.toUpperCase();
+
+const lastLetterCapPart2 = lastLetterPart2.toUpperCase();
+
+const remainingLettersPart1 = part1.slice(part1[0], part1.length - 1);
+
+const remainingLettersPart2 = part2.slice(part2[0], part2.length - 1);
 
 // Print the cameLtaiL-formatted string
-
-
-
+console.log(
+  `${remainingLettersPart1}${lastLetterCapPart1}${remainingLettersPart2}${lastLetterCapPart2}`
+);
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
@@ -35,23 +43,21 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+let tip = 84 * 0.15;
 
 // Print out the tipAmount
-
-
-
+console.log(tip);
 
 /*******************************************
     Iteration 2.2 | Generate Random Number
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.random() * (11 - 1) + 1;
+const intRandomNumber = Math.floor(randomNumber);
 
 // Print the generated random number
-
-
+console.log(intRandomNumber);
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -62,15 +68,22 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+// false
 
 const expression2 = a || b;
+//true
 
 const expression3 = !a && b;
+//false
 
 const expression4 = !(a && b);
+//true
 
 const expression5 = !a || !b;
+//true
 
 const expression6 = !(a || b);
+//false
 
 const expression7 = a && a;
+//true
