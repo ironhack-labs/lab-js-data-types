d37bce195c6a8652150c4818d635f048b84bba23
ecemonkol <ecemonkol@gmail.com>
2024-03-27 14:31:16 +0100

    Solved lab

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
+function concatenate() {
+    return `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`
+}
 
 
 // Print out the concatenated string
-
+console.log(concatenate());
 
 
 
@@ -22,10 +25,15 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+function concatenateAndUpperCase() {
+  const modifiedPart1  = part1.slice(0, part1.length -1) + part1[part1.length -1].toUpperCase();
+  const modifiedPart2  = part2.slice(0, part2.length -1) + part2[part2.length -1].toUpperCase();
+  const result = modifiedPart1 + modifiedPart2;
+  return result;
+}
 
 // Print the cameLtaiL-formatted string
-
+console.log(concatenateAndUpperCase())
 
 
 
@@ -35,10 +43,13 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+function calculateTip() {
+    tip = (billTotal / 100 ) * 15;
+    return tip;
+}
 
 // Print out the tipAmount
-
+console.log(calculateTip())
 
 
 
@@ -47,10 +58,13 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+function  generateNumber() {
+    const number = Math.ceil(Math.random() * 10);
+    return number;
+}
 
 // Print the generated random number
-
+console.log(generateNumber());
 
 
 /*******************************************
@@ -74,3 +88,11 @@ const expression5 = !a || !b;
 const expression6 = !(a || b);
 
 const expression7 = a && a;
+
+console.log(expression1); //false
+console.log(expression2); //true
+console.log(expression3); //false
+console.log(expression4); //true
+console.log(expression5); //true
+console.log(expression6); //false
+console.log(expression7); //true
